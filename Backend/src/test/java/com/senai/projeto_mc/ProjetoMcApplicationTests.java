0d5c63a0e55b9cc8799c4b1d896de76a00cadc83
mnorbutas <mnorbutass@gmail.com>
2025-12-05 10:25:53 -0300

    Envio Backend

---
+++ Backend/src/test/java/com/senai/projeto_mc/ProjetoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.projeto_mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
